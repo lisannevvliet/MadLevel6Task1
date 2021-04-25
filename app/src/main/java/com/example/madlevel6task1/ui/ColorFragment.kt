600dd7f78fe76672d82f7459e0506930cb179cf7
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2021-04-25 15:50:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madlevel6task1/ui/ColorFragment.kt
@@ -0,0 +1,55 @@
+package com.example.madlevel6task1.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel6task1.adapter.ColorAdapter
+import com.example.madlevel6task1.databinding.FragmentColorBinding
+import com.example.madlevel6task1.model.ColorItem
+import com.example.madlevel6task1.vm.ColorViewModel
+import com.google.android.material.snackbar.Snackbar
+
+// A simple [Fragment] subclass as the default destination in the navigation.
+class ColorFragment : Fragment() {
+    private val colors = arrayListOf<ColorItem>()
+    private lateinit var colorAdapter: ColorAdapter
+    private val viewModel: ColorViewModel by viewModels()
+    private var _binding: FragmentColorBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment.
+        _binding = FragmentColorBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        colorAdapter = ColorAdapter(colors, ::onColorClick)
+        binding.rvColors.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        binding.rvColors.adapter = colorAdapter
+
+        observeColors()
+    }
+
+    private fun observeColors() {
+        viewModel.colorItems.observe(viewLifecycleOwner, {
+            colors.clear()
+            colors.addAll(it)
+            colorAdapter.notifyDataSetChanged()
+        })
+    }
+
+    private fun onColorClick(colorItem: ColorItem) {
+        Snackbar.make(binding.rvColors, "This color is: ${colorItem.name}", Snackbar.LENGTH_LONG).show()
+    }
+}
---
+++ app/src/main/java/com/example/madlevel6task1/ui/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.madlevel6task1.ui
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.madlevel6task1.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+}
